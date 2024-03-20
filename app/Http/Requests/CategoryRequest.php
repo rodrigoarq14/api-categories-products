70cb6076aed0abfb47f143b014496050fa4cb9bc
root <root@rodrigo140599.localdomain>
2024-03-19 21:44:52 -0500

    fix;

--- app/Http/Requests/CategoryRequest.php
+++ app/Http/Requests/CategoryRequest.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Requests;
 
+use App\Constants\HttpResponseStatusCode;
+use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
 
 class CategoryRequest extends FormRequest
 {
@@ -25,5 +28,36 @@ class CategoryRequest extends FormRequest
             'name' => 'required|string|max:255',
             'description' => 'required|string|max:255'
         ];
+    }
+
+    /**
+     * Get the error messages for the defined validation rules.
+     *
+     * @return array<string, string>
+     */
+    public function messages(): array
+    {
+        return [
+            'name.required' => 'Name is required.',
+            'name.string' => 'Name must be a string.',
+            'name.max' => 'Name may not be greater than 255 characters.',
+            'description.required' => 'Description is required.',
+            'description.string' => 'Description must be a string.',
+            'description.max' => 'Description may not be greater than 255 characters.'
+        ];
+    }
+    
+    /**
+     * Handle a failed validation attempt.
+     * 
+     * @param \Illuminate\Contracts\Validation\Validator $validator
+     */
+    public function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            'success' => false,
+            'message' => 'Validation error.',
+            'data' => $validator->errors()
+        ], HttpResponseStatusCode::HTTP_BAD_REQUEST));
     }
 }
--- app/Http/Requests/ProductRequest.php
+++ app/Http/Requests/ProductRequest.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Requests;
 
+use App\Constants\HttpResponseStatusCode;
+use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
 
 class ProductRequest extends FormRequest
 {
@@ -28,5 +31,44 @@ class ProductRequest extends FormRequest
             'stock' => 'required|numeric|min:1',
             'category_id' => 'required|exists:categories,id'
         ];
+    }
+
+    /**
+     * Get the error messages for the defined validation rules.
+     *
+     * @return array<string, string>
+     */
+    public function messages(): array
+    {
+        return [
+            'name.required' => 'Name is required.',
+            'name.string' => 'Name must be a string.',
+            'name.max' => 'Name may not be greater than 255 characters.',
+            'description.required' => 'Description is required.',
+            'description.string' => 'Description must be a string.',
+            'description.max' => 'Description may not be greater than 255 characters.',
+            'price.required' => 'Price is required.',
+            'price.numeric' => 'Price must be a number.',
+            'price.min' => 'Price must be at least 1.',
+            'stock.required' => 'Stock is required.',
+            'stock.numeric' => 'Stock must be a number.',
+            'stock.min' => 'Stock must be at least 1.',
+            'category_id.required' => 'Category is required.',
+            'category_id.exists' => 'Category does not exist.'
+        ];
+    }
+
+    /**
+     * Handle a failed validation attempt.
+     * 
+     * @param \Illuminate\Contracts\Validation\Validator $validator
+     */
+    public function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            'success' => false,
+            'message' => 'Validation error.',
+            'data' => $validator->errors()
+        ], HttpResponseStatusCode::HTTP_BAD_REQUEST));
     }
 }
